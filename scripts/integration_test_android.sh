b24a52d9972c3b90927e581332ec18659d2bd3df
Rob Murphy <rmurphyswimmer@gmail.com>
2025-07-27 09:18:37 +0200

    fix: unset xdg config

--- scripts/integration_test_android.sh
+++ scripts/integration_test_android.sh
@@ -8,28 +8,26 @@ unset XDG_CONFIG_HOME
 
 echo "no" | avdmanager create avd --force -n test -k "system-images;android-35;google_apis_playstore;x86_64" --device "pixel_xl"
 
-avdmanager list avd
-#
-# emulator -avd test -no-snapshot-load -no-window &
-#
-# EMULATOR_PID=$!
-#
-# errorhandler () {
-#     kill $EMULATOR_PID
-# }
-# trap errorhandler ERR EXIT
-#
-# adb wait-for-device
-#
-# BOOT_COMPLETED=""
-# while [ "$BOOT_COMPLETED" != "1" ]; do
-#     sleep 5 
-#     BOOT_COMPLETED=$(adb shell getprop sys.boot_completed | tr -d '\r')
-#     echo "Boot status: $BOOT_COMPLETED (1 means ready)"
-# done
-#
-# echo "Emulator is ready!"
-#
-# adb install build_preview.apk
-#
-# maestro test .maestro/android
+emulator -avd test -no-snapshot-load -no-window &
+
+EMULATOR_PID=$!
+
+errorhandler () {
+    kill $EMULATOR_PID
+}
+trap errorhandler ERR EXIT
+
+adb wait-for-device
+
+BOOT_COMPLETED=""
+while [ "$BOOT_COMPLETED" != "1" ]; do
+    sleep 5 
+    BOOT_COMPLETED=$(adb shell getprop sys.boot_completed | tr -d '\r')
+    echo "Boot status: $BOOT_COMPLETED (1 means ready)"
+done
+
+echo "Emulator is ready!"
+
+adb install build_preview.apk
+
+maestro test .maestro/android
