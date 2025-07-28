a1cd9c83a1c0ddce5efe22b9214e9836a50d3b94
Rob Murphy <rmurphyswimmer@gmail.com>
2025-07-28 22:07:56 +0200

    feat: made workout page nicer

--- app/(tabs)/workout.tsx
+++ app/(tabs)/workout.tsx
@@ -1,6 +1,7 @@
 import { useRouter, Router, useLocalSearchParams } from "expo-router";
-import { FAB } from "react-native-paper";
-import { Text, View } from "react-native";
+import { Card, FAB, Surface } from "react-native-paper";
+import { View } from "react-native";
+import { Text } from "react-native-paper";
 interface WorkoutScreenProps {
   onAddWorkout: (r: Router) => void;
   selectedExercise: string | null;
@@ -16,9 +17,15 @@ export default function WorkoutScreen({
   const exercise = selectedExercise ?? exerciseSearchParam;
 
   return (
-    <View>
-      <Text>{exercise}</Text>
+    <View style={{ flex: 1, position: "relative" }}>
+      <Surface elevation={0} style={{ padding: 16 }}>
+        <Card>
+          {" "}
+          <Card.Title title={exercise} />
+        </Card>
+      </Surface>
       <FAB
+        style={{ position: "absolute", margin: 16, right: 0, bottom: 0 }}
         icon="plus"
         testID="add-workout"
         onPress={(_) => onAddWorkout(router)}
