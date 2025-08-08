dde57aab85f6a24832fa189442618713d06e1ae4
Rob Murphy <rmurphyswimmer@gmail.com>
2025-08-08 18:46:18 +0200

    feat: use web sdk for web setup

--- app/initialise_firebase.ts
+++ app/initialise_firebase.ts
@@ -1,9 +1,19 @@
-import { initializeApp } from '@react-native-firebase/app';
 import { Platform } from 'react-native';
-import firestore from '@react-native-firebase/firestore';
-import auth from '@react-native-firebase/auth';
 
-// web requires dynamic initialization on web prior to using firebase
+// Mobile SDK
+import firestoreMobile from '@react-native-firebase/firestore';
+
+// Web SDK
+import {
+	initializeApp as initializeWebApp
+} from 'firebase/app';
+import {
+	getFirestore as getFirestoreWeb,
+	connectFirestoreEmulator as connectFirestoreEmulatorWeb
+} from 'firebase/firestore';
+
+let db: any;
+
 if (Platform.OS === 'web') {
 	const firebaseConfig = {
 		apiKey: "AIzaSyALhajWDEhiAhJl0F_R5aB98fwUSgiHoos",
@@ -12,12 +22,19 @@ if (Platform.OS === 'web') {
 		storageBucket: "strengthassistantdev.firebasestorage.app",
 		messagingSenderId: "969424335861",
 		appId: "1:969424335861:web:395e0b79cf332b2e8b66bc",
-		measurementId: "G-1DN9PL58HD"
+		measurementId: "G-1DN9PL58HD",
+		databaseURL: "dummy"
 	};
-	initializeApp(firebaseConfig);
+	const app = initializeWebApp(firebaseConfig);
+	db = getFirestoreWeb(app);
+	if (__DEV__) {
+		connectFirestoreEmulatorWeb(db, 'localhost', 8080);
+	}
+} else {
+	db = firestoreMobile();
+	if (__DEV__) {
+		db.useEmulator('localhost', 8080); // RNFB native way
+	}
 }
 
-if (__DEV__) {
-	firestore().useEmulator('localhost', 8080);
-	auth().useEmulator('http://localhost:9099')
-}
+export { db };
--- lib/repo/ExerciseRepo.ts
+++ lib/repo/ExerciseRepo.ts
@@ -1,17 +1,17 @@
-import firestore from '@react-native-firebase/firestore';
+import firestore, { getFirestore } from '@react-native-firebase/firestore';
 import { Exercise } from "../models/Exercise";
 
 
 export class ExerciseRepo {
 
 	addExercise = async (exercise: string): Promise<string> => {
-		const testCollection = firestore().collection('Test');
+		const testCollection = getFirestore().collection('Test');
 		const doc = await testCollection.add({ name: exercise });
 		return doc.id;
 	};
 
 	getExerciseById = async (id: string): Promise<Exercise | undefined> => {
-		const testCollection = firestore().collection('Test');
+		const testCollection = getFirestore().collection('Test');
 		const data = await testCollection.doc(id).get().then((snap) => {
 			const data = snap.data();
 			if (data === undefined) {
