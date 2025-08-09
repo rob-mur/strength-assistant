ed8a54737aa7a3283e79430e3d99fd5b2bf8e9b3
Rob Murphy <rmurphyswimmer@gmail.com>
2025-08-09 15:43:41 +0200

    test: working but complex

--- __tests__/integration/on-add-exercise-navigation-test.tsx
+++ __tests__/integration/on-add-exercise-navigation-test.tsx
@@ -1,32 +1,51 @@
-import { CommonTestState } from "../../__test_utils__/utils";
-import AddExerciseForm from "@/lib/components/Forms/AddExerciseForm";
-import WorkoutScreen from "@/app/(tabs)/workout";
-import ExerciseScreen from "@/app/(tabs)/exercises";
+// import { CommonTestState } from "../../__test_utils__/utils";
+// import AddExerciseForm from "@/lib/components/Forms/AddExerciseForm";
+// import WorkoutScreen from "@/app/(tabs)/workout";
+// import ExerciseScreen from "@/app/(tabs)/exercises";
+// import { renderRouter, screen } from "expo-router/testing-library";
+//
+// describe("navigation", () => {
+//   let state: CommonTestState;
+//   beforeEach(() => {
+//     state = new CommonTestState();
+//   });
+//   test("When the user adds an exercise they are returned to the workout screen", async () => {
+//     // Given
+//     renderRouter(
+//       {
+//         "(tabs)/exercises": () => <ExerciseScreen />,
+//
+//         "(tabs)/exercises/add": () => <AddExerciseForm />,
+//         "(tabs)/workout": () => <WorkoutScreen />,
+//       },
+//       { initialUrl: "/(tabs)/exercises/add" },
+//     );
+//
+//     // When
+//     await state.user.type(screen.getByTestId("name"), "Exercise Name");
+//     await state.user.press(screen.getByTestId("submit"));
+//     await state.user.press(screen.getByTestId("exercisesTab"));
+//
+//     // Then
+//     expect(screen).toHavePathname("/(tabs)/exercises");
+//   });
+// });
 import { renderRouter, screen } from "expo-router/testing-library";
+import { View } from "react-native";
 
-describe("navigation", () => {
-  let state: CommonTestState;
-  beforeEach(() => {
-    state = new CommonTestState();
-  });
-  test("When the user adds an exercise they are returned to the workout screen", async () => {
-    // Given
-    renderRouter(
-      {
-        "(tabs)/exercises": () => <ExerciseScreen />,
+it("my-test", async () => {
+  const MockComponent = jest.fn(() => <View />);
 
-        "(tabs)/exercises/add": () => <AddExerciseForm />,
-        "(tabs)/workout": () => <WorkoutScreen />,
-      },
-      { initialUrl: "/(tabs)/exercises/add" },
-    );
+  renderRouter(
+    {
+      index: MockComponent,
+      "directory/a": MockComponent,
+      "(group)/b": MockComponent,
+    },
+    {
+      initialUrl: "/directory/a",
+    },
+  );
 
-    // When
-    await state.user.type(screen.getByTestId("name"), "Exercise Name");
-    await state.user.press(screen.getByTestId("submit"));
-    await state.user.press(screen.getByTestId("exercisesTab"));
-
-    // Then
-    expect(screen).toHavePathname("/(tabs)/exercises");
-  });
+  expect(screen).toHavePathname("/directory/a");
 });
