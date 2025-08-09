ec478c3fd01078500b6e2a5b74e6d316c758da20
Rob Murphy <rmurphyswimmer@gmail.com>
2025-08-09 17:03:05 +0200

    fix: mock firebase in integration

--- __tests__/integration/on-add-exercise-navigation-test.tsx
+++ __tests__/integration/on-add-exercise-navigation-test.tsx
@@ -1,3 +1,9 @@
+jest.mock("@/app/initialise_firebase", () => ({
+  // Mock the functions or exports from initialise_firebase.ts here.
+  // For example, if it exports a function named `initialize`, you can mock it like this:
+  initFirebase: jest.fn(),
+}));
+
 import { CommonTestState } from "../../__test_utils__/utils";
 import AddExerciseForm from "@/lib/components/Forms/AddExerciseForm";
 import WorkoutScreen from "@/app/(tabs)/workout";
@@ -5,6 +11,7 @@ import ExerciseScreen from "@/app/(tabs)/exercises";
 import { renderRouter, screen } from "expo-router/testing-library";
 import RootLayout from "@/app/_layout";
 import { preventAutoHideAsync } from "expo-router/build/utils/splash";
+import { initFirebase } from "@/app/initialise_firebase";
 
 jest.mock("expo-router", () => {
   const originalModule = jest.requireActual("expo-router");
