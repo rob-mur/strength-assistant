1365dd1916ca980142201214abfb8c565003c488
Rob Murphy <58891480+rob-mur@users.noreply.github.com>
2025-08-15 15:19:52 +0200

    Refactor build system and workflows (#41)
    * test: try making build with github
    
    * Revert "chore: give claude access to workflows write"
    
    This reverts commit 570e4eccac4d10f4ce1191b65679cb5721dd852c.
    
    * Reapply "chore: give claude access to workflows write"
    
    This reverts commit b0e2f2555f4ac484575242e0ede62b73280ecd75.
    
    * fix: issues
    
    * fix: issues

--- metro.config.js
+++ metro.config.js
@@ -10,22 +10,21 @@ const fs = require("fs");
 const path = require("path");
 
 config.resolver.resolveRequest = (context, moduleName, platform) => {
-  if (platform === "android" || platform === "ios") {
-    const moduleFolder = moduleName.substring(2);
-    const webFileName = `${moduleFolder}/${path.basename(moduleFolder)}.web.ts`;
-    const nativeFileName = `${moduleFolder}/${path.basename(moduleFolder)}.native.ts`;
-    if (moduleName.startsWith("@/") && fs.existsSync(webFileName)) {
-      if (!fs.existsSync(nativeFileName)) {
-        throw Error(`Expected native file for ${moduleFolder}`);
-      }
-      return {
-        filePath: `${process.cwd()}/${nativeFileName}`,
-        type: "sourceFile",
-      };
+  const moduleFolder = moduleName.substring(2);
+  const webFileName = `${moduleFolder}/${path.basename(moduleFolder)}.web.ts`;
+  const nativeFileName = `${moduleFolder}/${path.basename(moduleFolder)}.native.ts`;
+  if (moduleName.startsWith("@/") && fs.existsSync(webFileName)) {
+    if (!fs.existsSync(nativeFileName)) {
+      throw Error(`Expected native file for ${moduleFolder}`);
     }
-
-    return context.resolveRequest(context, moduleName, platform);
+    const fileName = platform === "web" ? webFileName : nativeFileName;
+    return {
+      filePath: `${process.cwd()}/${fileName}`,
+      type: "sourceFile",
+    };
   }
+
+  return context.resolveRequest(context, moduleName, platform);
 };
 
 module.exports = withStorybook(config, {
