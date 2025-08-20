ab937c4d5e907364191066aef8bf1a0e3698ba50
Rob Murphy <rmurphyswimmer@gmail.com>
2025-08-20 12:27:13 +0200

    fix: minor issues

--- scripts/clear_emulator.js
+++ scripts/clear_emulator.js
@@ -1,5 +1,5 @@
 // There is a magic import for maestro
 // eslint-disable-next-line no-undef
 http.delete(
-  "http://localhost:8080/emulator/v1/projects/strengthassistantdev/databases/(default)/documents",
+  "http://localhost:4400/emulator/v1/projects/strengthassistantdev/databases/(default)/documents",
 );
