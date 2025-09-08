509f675a1df76aab4b48f5ab77170f727e4bfbbe
Rob Murphy <rmurphyswimmer@gmail.com>
2025-09-08 14:27:09 +0200

    chore: sync devbox

--- devbox/dev/.devbox/gen/scripts/test.sh
+++
@@ -1,7 +0,0 @@
-set -e
-
-if [ -z "$__DEVBOX_SKIP_INIT_HOOK_b24fe463b612d47a5dd55b7d14eb6c908a024328c178daf08f77c3ed16779bde" ]; then
-    . "/home/droid/repos/strength-assistant/devbox/dev/.devbox/gen/scripts/.hooks.sh"
-fi
-
-../../scripts/test.sh
